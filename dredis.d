9c73925b1356d17793c806ff37106be3d291973e
zhaopuming <zhaopuming@gmail.com>
2012-01-09 15:57:26 +0800

    Support for IsThrow and NotThrow options

--- dredis.d
+++ dredis.d
@@ -9,47 +9,139 @@ struct RedisReply {
   string str;
 }
 
-class Redis {
-  private redisContext* context;
-
-  public void connect(const(char*) host, int ip, int timeout) {
-    timeval ts = {1, timeout * 1000};
-    context = redisConnectWithTimeout(host, ip, ts);
-  }
-
-  /****************************
-   * String Operations
-   ***************************/
-
-  public bool set(const(char)[] key, const(char)[] val) {
-    return boolCmd("SET", key, val);
-  }
-
-  public string get(const(char)[] key) {
-    redisReply* rep = cast(redisReply*) redisCommand(context, "GET %s", toStringz(key));
-    string s = to!string(rep.str);
-    freeReplyObject(rep);
-    return to!string(s);
-  }
-
-  public bool del(const(char)[] key) {
-    return boolCmd("DEL", key);
-  }
-
-  private bool boolCmd(string cmd, const(char)[] key, const(char)[] val = "") {
-    redisReply* rep = cast(redisReply*) redisCommand(context, toStringz(cmd ~ " %s %s"), toStringz(key), toStringz(val));
-    freeReplyObject(rep);
-    // TODO: check whether successfull
-    return true;
+class DredisException : Exception {
+  this(string s) {
+    super(s);
   }
 }
 
+template DredisTemplate(bool isThrow)
+{
+  class Redis
+  {
+    private redisContext* context;
+    // TODO: Mayby default values should be config as template args
+    private string defaultString = "nil";
+    private long defaultNumber = 0L;
+
+    public void setDefaultValue(string str)
+    {
+      defaultString = str;
+    }
+
+    public void setDefaultNumber(long number)
+    {
+      defaultNumber = number;
+    }
+
+    public redisContext* getContext()
+    {
+      return context;
+    }
+
+    public void connect(const(char*) host, int ip, int timeout)
+    {
+      timeval ts = {1, timeout * 1000};
+      context = redisConnectWithTimeout(host, ip, ts);
+    }
+
+    /****************************
+    * String Operations
+    ***************************/
+
+    public bool set(const(char)[] key, const(char)[] val)
+    {
+      return boolCmd("SET", key, val);
+    }
+
+    public string get(const(char)[] key)
+    {
+      redisReply* rep = cast(redisReply*) redisCommand(context, "GET %s", toStringz(key));
+      string s;
+      switch (rep.type)
+      {
+        case REDIS_REPLY_STRING:
+          s = to!string(rep.str);
+          break;
+        case REDIS_REPLY_ERROR:
+          static if (isThrow)
+          {
+            throw new DredisException("haha");
+          }
+          else
+          {
+            s = defaultString;
+            break;
+          }
+        default:
+          s = defaultString;
+      }
+      freeReplyObject(rep);
+      return s;
+    }
+
+    public long getLong(const(char)[] key)
+    {
+      redisReply* rep = cast(redisReply*) redisCommand(context, "GET %s", toStringz(key));
+      long s;
+      switch (rep.type)
+      {
+        case REDIS_REPLY_INTEGER:
+          s = rep.integer;
+        case REDIS_REPLY_STRING:
+          // find an efficient way to conver char* to long
+          // s = to!long(to!string(rep.str));
+          s = std.c.stdlib.atoi(rep.str);
+          break;
+        case REDIS_REPLY_ERROR:
+          // TODO: when in ExcMode, throw an exception
+          static if (isThrow) {
+            throw new DredisException("ahah");
+          } else {
+          s = defaultNumber;
+          }
+          break;
+        default:
+          s = defaultNumber;
+          break;
+      }
+      freeReplyObject(rep);
+      return s;
+    }
+
+    public bool del(const(char)[] key)
+    {
+      return boolCmd("DEL", key);
+    }
+
+    /**
+    * Common implementation of boolean commands
+    * TODO: should support varargs
+    **/
+    private bool boolCmd(string cmd, const(char)[] key, const(char)[] val = "")
+    {
+      redisReply* rep = cast(redisReply*) redisCommand(context, toStringz(cmd ~ " %s %s"), toStringz(key), toStringz(val));
+      bool succ = true;
+      if (rep.type == REDIS_REPLY_ERROR) {
+        succ = false;
+      }
+      freeReplyObject(rep);
+      return succ;
+    }
+  }
+
+}
+
+alias DredisTemplate!true.Redis Dredis;
+alias DredisTemplate!false.Redis FastDredis;
+
 void main() {
-  auto r = new Redis();
+  auto r = new Dredis();
   r.connect("127.0.0.1", 6379, 5000);
   writeln(r.get("foo"));
   r.set("haha", "hooho");
   writeln(r.get("haha"));
   r.del("haha");
   writeln(r.get("mylist"));
+  writeln(r.getLong("a"));
 }
